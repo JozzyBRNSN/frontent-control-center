ad9664fca403bfbe31849f581e7c0d645b4787a3
Dzmitry Asipovich <d.osipovich@itgrade.ru>
2025-12-30 16:30:11 +0300

    ЗАДАНИЕ 1.2 — Добавление элемента

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,40 @@
 export default {
 	data() {
 		return {
-			title: "Счетчик",
-			counter: 0,
+			// title: "Счетчик",
+			// counter: 0,
+			items: [
+				{ id: 1, title: "Доделать таску" },
+				{ id: 2, title: "Закрыть день" },
+				{ id: 3, title: "Спать" },
+			],
+			value: "",
 		};
+	},
+	methods: {
+		addNewItem() {
+			if (this.value.trim() === "") {
+				return;
+			}
+			const maxId = this.items.length
+				? Math.max(...this.items.map(item => item.id))
+				: 0;
+			const newId = maxId + 1;
+			this.items.push({ id: newId, title: this.value });
+			this.value = "";
+		},
 	},
 };
 </script>
 
 <template>
-	<h1>{{ title }}</h1>
+	<!-- <h1>{{ title }}</h1>
 	<br />
 	<div>{{ counter }}</div>
 	<button @click="counter--">-</button>
 	<button @click="counter++">+</button>
+	<hr /> -->
+	<li v-for="item in items" :key="item.id">{{ item.id }}. {{ item.title }}</li>
+	<input type="text" v-model="value" />
+	<button @click="addNewItem">Добавить</button>
 </template>
-
